f30b3c41e96fded1f7856dbf98fddf050e95f38f
lukakhonelidze <anriaa6@gmail.com>
2025-03-30 14:41:49 +0400

    third hackerscript.js

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ console.log(isEven(2));  // true
 console.log(isEven(3));  // false
 console.log(isEven(10)); // true
 console.log(isEven(7));  // false
+
+//player 3
+
+function sumOfDigits(num){
+    let sum = 0
+    while(num !== 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10)
+    }
+    return Math.abs(sub)
+}
+
+console.log(sumOfDigits(123));
+console.log(sumOfDigits(987));
+console.log(sumOfDigits(0));
+console.log(sumOfDigits(-456));
