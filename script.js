7ef82d886a1f7759cf0e1253a81203f9df6a7ba9
Grafi1967 <zgarbaqobalia@gmail.com>
2025-03-30 14:36:45 +0400

    second hacker script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function isPrime(num) {
     return true;
 }
 console.log(isPrime(7));
+
+//player 2
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+// ტესტირება
+console.log(isEven(2));  // true
+console.log(isEven(3));  // false
+console.log(isEven(10)); // true
+console.log(isEven(7));  // false
